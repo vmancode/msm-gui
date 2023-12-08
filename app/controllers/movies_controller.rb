d2a0d4158f2261680601e0698372bbd717614673
vmancode <146589771+vmancode@users.noreply.github.com>
2023-12-08 02:37:41 +0000

    commit2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,30 @@
 class MoviesController < ApplicationController
+  def update
+    # Get the ID out of params
+    m_id = params.fetch("the_id")
+
+    # Look up the existing record
+    matching_records = Movie.where({ :id => m_id })
+    the_movie = matching_records.at(0)
+
+    # Overwrite each column with the values from user inputs
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    # Save
+
+    the_movie.save
+
+    # Redirect to the movie details page
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
+
+  
   def create
     # params hash looks like this:
     # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
@@ -22,6 +48,20 @@ class MoviesController < ApplicationController
 
     # Redirect the user back to the /movies URL
   end
+
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
+
 
 
   def index
